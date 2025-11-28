481fa4ec425658844365c7c6cc106811a689038e
Javier <gutifer666@gmail.com>
2025-11-28 14:42:57 +0100

    añadidos tests false

--- tests/test_suit_unittest_charfun.py
+++ tests/test_suit_unittest_charfun.py
@@ -7,12 +7,13 @@ class TestSuitUnitTestCharFun(unittest.TestCase):
         # Casos que deben devolver True
         casos_true = [
             "oso",                 # minúsculas
+            "Anita lava la tina",  # palíndromo con espacios y mayúsculas
             "osO",                 # mixto mayúsc/minúsc
             "0s0",                 # alfanumérico
             "",                    # cadena vacía
             " os o ",              # con espacios
             "'!o?s,o.;:?",         # con puntuación
-            "@o#s&o$\\",          # con símbolos especiales (ojo con el backslash)
+            "@o#s&o$\\",          # con símbolos especiales
             "+-0*s%=0&&||",        # con símbolos matemáticos
             "osó",                 # unicode con tilde
         ]
@@ -24,7 +25,9 @@ class TestSuitUnitTestCharFun(unittest.TestCase):
         # Casos que deben devolver False
         casos_false = [
             "osa",
-            # añade más si quieres: "abc", "ab", "Python", ...
+            "Anita lava las tina",  # casi palíndromo,
+            "!?oSa';:",             # no palíndromo con puntuación
+            "osá"                   # no palindromo con tilde
         ]
         for s in casos_false:
             with self.subTest(caso=s):
